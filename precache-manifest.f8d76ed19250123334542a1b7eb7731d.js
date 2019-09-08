7c3fa322b4d939e018b258f66893be416953e9ff
codeblad3 <code.blad3@gmail.com>
2019-09-07 23:03:40 -0300

    Updates

---
+++ precache-manifest.f8d76ed19250123334542a1b7eb7731d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3048a74c758edd0ed222b7f510f16bac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "364c6f5a7c7a234f6cba",
+    "url": "/monsters-rolodex/static/css/main.3e7bc1fe.chunk.css"
+  },
+  {
+    "revision": "89375460eeff67c8d98d",
+    "url": "/monsters-rolodex/static/js/2.2752c3e5.chunk.js"
+  },
+  {
+    "revision": "364c6f5a7c7a234f6cba",
+    "url": "/monsters-rolodex/static/js/main.4b50b023.chunk.js"
+  },
+  {
+    "revision": "9679b25fbb58365eb7b7",
+    "url": "/monsters-rolodex/static/js/runtime~main.c610535c.js"
+  }
+]);
